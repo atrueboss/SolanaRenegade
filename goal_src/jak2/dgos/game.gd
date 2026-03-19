57561c23823e93262834fd765280735bf1655a2e
atrueboss <154102898+atrueboss@users.noreply.github.com>
2026-03-19 20:40:23 +0900

    added mod-sound with various sound functions
    -custom music on spawn
    -added all rac1 original ost
    -added in-bubble-m? function
    -added volume manager
    -change music on trigger function(it's bad tho, will fix later)

--- goal_src/jak2/dgos/game.gd
+++ goal_src/jak2/dgos/game.gd
@@ -474,6 +474,7 @@
   "mod-settings.o" ;; added ;;mod-base-change
   "mod-common-functions.o" ;; added
   "orb-placer.o"  ;; added
+  "mod-sound.o" ;; added in solana renegade mod
   "mod-custom-code.o" ;; added
   "mod-debug.o" ;; added
  ))
